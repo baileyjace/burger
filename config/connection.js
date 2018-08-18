663fd35b7ba595069a690cd0ac7a536a709180bb
Bailey Jace <baileyjace@gmail.com>
2018-08-18 12:58:58 -0500

    updated jawsdURL

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,17 @@ var mysql = require('mysql');
 
 var connection;
 
+if (process.env.JAWSDB_URL) {
+    console.log(process.env);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
     connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: 'root',
         database: 'burgers_db'
     });
-
+};
 
 connection.connect(function (err) {
     if (err) {
